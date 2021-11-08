8ff2cb446b9d37f8f4f56e69e63e5cdee3cd90d6
eugen1701 <eugen.flocea@yahoo.com>
2021-11-08 11:51:10 +0200

    Is working

---
+++ src/FiniteAutomata.java
@@ -0,0 +1,143 @@
+import javafx.util.Pair;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class FiniteAutomata {
+
+    private List<String> Q; // set of states
+    private List<String> E; //set of input symbols
+    private String q0; //initial state
+    private List<String> F; //set of final state
+    private Map<Pair<String, String>, ArrayList<String>> S; // transition function (S : Q X E --> Q)
+
+
+    public List<String> readLineToList(String line){
+        String array[] = line.split(" ");
+        return Arrays.asList(array.clone());
+    }
+
+    public FiniteAutomata() {
+        this.S = new HashMap<>();
+    }
+
+    public void read(String inputFile) {
+        File file = new File(inputFile);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Q = readLineToList(scanner.nextLine());
+        E = readLineToList(scanner.nextLine());
+        q0 = scanner.nextLine();
+        F = readLineToList(scanner.nextLine());
+
+        while(scanner.hasNextLine()) {
+            List<String> line = readLineToList(scanner.nextLine());
+            Pair<String, String> key = new Pair<>(line.get(0), line.get(1));
+            if(!S.containsKey(key)){ // we look if the key is already added
+                ArrayList<String> value = new ArrayList<>();
+                value.add(line.get(2)); // if not we create a list with the element and put it in S with the new key
+                S.put(key, value);
+            }
+            else{ // if the key already exists we just ad the element to the existing element
+                S.get(key).add(line.get(2));
+            }
+
+        }
+
+        scanner.close();
+    }
+
+    public boolean isValid() {
+        if(!Q.contains(q0))
+            return false;
+        for(String state: F){
+            if(!Q.contains(state))
+                return false;
+        }
+
+        for(Map.Entry el : S.entrySet()) {
+            Pair<String, String> key = (Pair<String, String>) el.getKey();
+            ArrayList<String> value = (ArrayList<String>) el.getValue();
+            if(!Q.contains(key.getKey()))
+                return false;
+            if(!E.contains((key.getValue())))
+                return false;
+            for(String dest : value) {
+                if(!Q.contains(dest))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isAccepted(String seq) {
+        if(this.isDfa()){
+            String current = q0;
+            for(int i = 0; i < seq.length(); i++){
+                char c = seq.charAt(i);
+                Pair<String, String> cp = new Pair<>(current, Character.toString(c));
+                if(S.keySet().contains(cp)) {
+                    current = S.get(cp).get(0);
+                }
+                else return false;
+            }
+            if(!F.contains(current))
+                return false;
+            else return true;
+        }
+        return false;
+    }
+
+    public boolean isDfa() {
+        for(Map.Entry el : S.entrySet()) {
+            ArrayList<String> value = (ArrayList<String>) el.getValue();
+            if(value.size()>1) return false;
+        }
+        return true;
+    }
+
+    public String displayStates() {
+        return Q.toString();
+    }
+
+    public String displayAlphabet() {
+        return E.toString();
+    }
+
+    public String displayInitialState() {
+        return q0;
+    }
+
+    public String displayFinalState() {
+        return F.toString();
+    }
+
+    public String displayTransition() {
+        StringBuilder toDislplay = new StringBuilder("S = { \n");
+
+        for(Map.Entry el : S.entrySet()) {
+            Pair<String, String> key = (Pair<String, String>) el.getKey();
+            toDislplay.append("(").append(key.getKey()).append(", ").append(key.getValue()).append(") -> ").append(el.getValue()).append("\n");
+        }
+        toDislplay.append("}");
+        return toDislplay.toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder toDislplay = new StringBuilder("FiniteAutomata{\n" +
+                "Q=" + Q + "\n" +
+                "E=" + E + "\n" +
+                "q0='" + q0 + '\'' + "\n" +
+                "F=" + F + "\n" );
+        toDislplay.append(this.displayTransition());
+        return toDislplay.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        FiniteAutomata fa = new FiniteAutomata();
+        while(true) {
+            displayMenu();
+            Scanner keyboard = new Scanner(System.in);
+            int cmd = keyboard.nextInt();
+            if(cmd == 1) {
+                System.out.println(fa);
+            }
+            if(cmd == 2) {
+                System.out.println(fa.displayStates());
+            }
+            if(cmd == 3) {
+                System.out.println(fa.displayAlphabet());
+            }
+            if(cmd == 4) {
+                System.out.println(fa.displayTransition());
+            }
+            if(cmd == 5) {
+                System.out.println(fa.displayInitialState());
+            }
+            if(cmd == 6) {
+                System.out.println(fa.displayFinalState());
+            }
+            if(cmd == 7) {
+                if(fa.isValid())
+                    System.out.println("TRUE, FA is valid");
+                else System.out.println("FALSE, Fa is not valid");
+            }
+            if(cmd == 8) {
+                if(fa.isDfa())
+                    System.out.println("TRUE, FA is deterministic");
+                else System.out.println("FALSE, FA is not deterministic");
+            }
+            if(cmd == 9) {
+                String seq = keyboard.nextLine();
+                seq = seq.strip();
+                if(fa.isAccepted(seq))
+                    System.out.println("YES, the seq " + seq + " is accepted by FA");
+                else System.out.println("NO, the seq " + seq + " is not accepted by FA");
+            }
+            if(cmd == 10) {
+                //try {
+                    fa.read("FA.in");
+               // }
+               // catch (Exception e) {
+               //     System.out.println("Finite automata couldn't be read" + e);
+              //  }
+            }
+            if(cmd == 0)
+                return;
 
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("1. Display FA ");
+        System.out.println("2. Display states ");
+        System.out.println("3. Display alphabet ");
+        System.out.println("4. Display transitions ");
+        System.out.println("5. Display initial states ");
+        System.out.println("6. Display final states ");
+        System.out.println("7. Check if FA is valid ");
+        System.out.println("8. Check if FA is deterministic ");
+        System.out.println("9. Check if a sequence is accepted by the FA ");
+        System.out.println("10. Read FA from file");
+        System.out.println("0. Exit ");
     }
 }
